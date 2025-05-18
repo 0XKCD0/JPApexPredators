d104a51978edbb9d5a0b3c217ae61c4c5fa9b45e
Shivangi Dutta <shivangidutta4175@gmail.com>
2025-05-19 00:34:59 +0530

    Updated project

--- JPApexPredators/PredatorMap.swift
+++ JPApexPredators/PredatorMap.swift
@@ -1,18 +1,18 @@
 //
-//  PredatorDetail.swift
+//  PredatorMap.swift
 //  JPApexPredators
 //
-//  Created by Shivangi Dutta on 16/05/25.
+//  Created by Shivangi Dutta on 18/05/25.
 //
 
 import SwiftUI
 
-struct PredatorDetail: View {
+struct PredatorMap: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    PredatorDetail()
+    PredatorMap()
 }
